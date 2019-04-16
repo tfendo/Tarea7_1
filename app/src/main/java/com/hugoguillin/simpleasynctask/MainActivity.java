437cd1e792de58017044fd95f4f0ab4c53577cbc
Hugo Guillín <hugoguillin@hotmail.com>
2019-04-16 12:41:06 +0200

    Task_3

--- app/src/main/java/com/hugoguillin/simpleasynctask/MainActivity.java
+++ app/src/main/java/com/hugoguillin/simpleasynctask/MainActivity.java
@@ -3,16 +3,31 @@ package com.hugoguillin.simpleasynctask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private static final String TEXT_STATE = "currentText";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView1);
+        if(savedInstanceState!=null){
+            textView.setText(savedInstanceState.getString(TEXT_STATE));
+        }
     }
 
     public void startTask(View view) {
+        textView.setText(getString(R.string.napping));
+        new SimpleAsyncTask(textView).execute();
+    }
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(TEXT_STATE, textView.getText().toString());
     }
 }
